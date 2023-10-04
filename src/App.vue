6089d585352597eea7fe27df76267ddd4756d04a
JaredRomy <JaredRomy@gmail.com>
2023-10-04 15:37:05 -0500

    Finished Part 2.

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,6 @@ import Form from './components/Form.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-  </header>
-
   <main>
     <Form />
   </main>
